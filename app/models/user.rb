cea7ec72a38fcc61de7514a7ee39d6f6d1aa8414
dry534 <apuro53.4@gmail.com>
2024-02-20 19:46:08 +0900

    バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          validates :nickname, presence: true
+         validates :birthday, presence: true
 
  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} do     
          validates :first_name, presence: true
