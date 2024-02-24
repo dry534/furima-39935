394cfd3d4f901a4385ced2caae8e9f5ca97516f9
dry534 <apuro53.4@gmail.com>
2024-02-25 04:11:44 +0900

    カテゴリー選択機能の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,15 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :categoty
+
+  validates :item_name, presence: true
+  validates :detail, presence: true
+  validates :price, presence: true
+  validates :category_id, numericality: { other_than: 1 } 
+  validates :situation_id, numericality: { other_than: 1 } 
+  validates :delivery_day_id, numericality: { other_than: 1 } 
+  validates :load_style_id, numericality: { other_than: 1 } 
+  validates :category_id, numericality: { other_than: 1 } 
+  validates :prefecture_id, numericality: { other_than: 1 } 
+
 end
