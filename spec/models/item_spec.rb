5a1d3d9ed6bd1feb06b74daa072a0c6c94d80a80
dry534 <apuro53.4@gmail.com>
2024-02-25 19:14:49 +0900

    テストコード分割

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -33,11 +33,13 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include "Price can't be blank"
       end
 
-      it '価格が300から9,999,999の間でないと登録できない' do
+      it '価格が300以上でないと登録できない' do
         @item.price = 299
         @item.valid?
         expect(@item.errors.full_messages).to include "Price must be greater than or equal to 300"
+      end
 
+      it '価格が9,999,999以下でないと登録できない' do
         @item.price = 10_000_000
         @item.valid?
         expect(@item.errors.full_messages).to include "Price must be less than or equal to 9999999"
