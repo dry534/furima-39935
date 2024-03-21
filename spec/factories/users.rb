99b7023b25fecc470b989e75aa6342b84fbcaa4d
dry534 <apuro53.4@gmail.com>
2024-03-21 17:23:34 +0900

    テストコード修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,16 +6,12 @@ FactoryBot.define do
     password_confirmation { password }
     first_name            { generate_full_width_character }
     family_name           { generate_full_width_character }
-    first_name_kana       { generate_full_width_kana }
-    family_name_kana      { generate_full_width_kana }
+    first_name_kana       { 'フガフガ' }
+    family_name_kana      { 'ホゲホゲ' }
     birthday              { Faker::Date.birthday }
   end
 end
 
 def generate_full_width_character
   (0x3041..0x3096).to_a.pack('U*').chars.sample(5).join
-end
-
-def generate_full_width_kana
-  (0x30A1..0x30F6).to_a.pack('U*').chars.sample(5).join
 end
