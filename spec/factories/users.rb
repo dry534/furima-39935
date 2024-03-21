b07ded47f6e47f5d6694270b4f11df5b076b33c9
dry534 <apuro53.4@gmail.com>
2024-03-21 19:53:07 +0900

    テストコード修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,14 +4,12 @@ FactoryBot.define do
     email                 { Faker::Internet.email }
     password              { Faker::Alphanumeric.alphanumeric(number: 8, min_numeric: 1) }
     password_confirmation { password }
-    first_name            { generate_full_width_character }
-    family_name           { generate_full_width_character }
+    first_name            { 'ふがふが' }
+    family_name           { 'ほげほげ' }
     first_name_kana       { 'フガフガ' }
     family_name_kana      { 'ホゲホゲ' }
     birthday              { Faker::Date.birthday }
   end
 end
 
-def generate_full_width_character
-  (0x3041..0x3096).to_a.pack('U*').chars.sample(5).join
-end
+
