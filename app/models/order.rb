034ccfebfc3af4fb21e4ddb2062760688d6a776c
dry534 <apuro53.4@gmail.com>
2024-03-21 22:29:05 +0900

    バリデーションの修正

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,11 +4,11 @@ class Order
   attr_accessor :post_code, :prefecture_id, :city, :block, :building_name, :phone_number, :user_id, :item_id, :token, :price, :buying_history_id
 
 
-  validates :post_code, :city, :block, :phone_number, :user_id, :item_id, presence: true
+  validates :post_code, :city, :block, :phone_number, :user_id, :item_id,:token, presence: true
   validates :post_code, format: { with: /\A\d{3}-\d{4}\z/ }
   validates :prefecture_id, numericality: { other_than: 1 } 
   validates :phone_number, format: { with: /\A\d{10,11}\z/ }
-  validates :token, presence: true
+
 
   def save
  order = BuyingHistory.create(item_id: item_id, user_id: user_id)
