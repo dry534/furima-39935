3f2c306b4c17969cb352cdeeb1261d409f594931
dry534 <apuro53.4@gmail.com>
2024-03-21 17:22:23 +0900

    マイグレーションファイルの修正

--- db/migrate/20240307122046_create_addresses.rb
+++ db/migrate/20240307122046_create_addresses.rb
@@ -4,7 +4,7 @@ class CreateAddresses < ActiveRecord::Migration[7.0]
       t.string :post_code,      null: false, default: ""
       t.integer :prefecture_id,  null: false, default: 0
       t.string :city,      null: false, default: ""
-      t.string :block
+      t.string :block,     null: false, default: ""
       t.string :building_name
       t.string :phone_number,  null: false, default: ""
       t.references :buying_history, null: false, foreign_key: true
