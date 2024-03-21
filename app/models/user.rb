e5370873d7a21a6b745a0af2591ce53d45c93f97
dry534 <apuro53.4@gmail.com>
2024-03-21 19:52:59 +0900

    アソシエーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+         has_many :buying_histories
+         has_many :items
+
          validates :nickname, presence: true
          validates :birthday, presence: true
 
